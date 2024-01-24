864dde5e055a8682cbcc39f8220b627a5bc32d6d
Drew Ansbacher <drew.ansbacher@clipboardhealth.com>
2024-01-24 18:53:45 +0200

    matter table done

--- src/modules/matters/matters.module.ts
+++ src/modules/matters/matters.module.ts
@@ -3,12 +3,11 @@ import { MatterController } from './matters.controller';
 import { MatterRepository } from './matters.repository';
 import { PrismaClient } from '@prisma/client';
 import { ServicesModule } from 'src/services/services.module';
-import { MatterStatusRepository } from './matters-status.repository';
 
 @Module({
   imports: [ServicesModule],
   controllers: [MatterController],
-  providers: [MatterRepository, MatterStatusRepository, PrismaClient],
-  exports: [MatterRepository, MatterStatusRepository],
+  providers: [MatterRepository, PrismaClient],
+  exports: [MatterRepository],
 })
 export class MatterModule {}
