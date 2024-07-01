5eb343b788a4da655a0f7428f13b81a2ecd88413
Drew Ansbacher <drew.ansbacher@clipboardhealth.com>
2024-07-01 11:46:26 +0200

    last used

--- src/modules/matters/matters.repository.ts
+++ src/modules/matters/matters.repository.ts
@@ -11,12 +11,45 @@ import {
 export class MatterRepository {
   constructor(private readonly prismaClient: PrismaClient) {}
 
-  findAll(): Promise<(Matter & { status: MatterStatus; client: Client })[]> {
-    return this.prismaClient.matter.findMany({
+  async findAll(): Promise<
+    (Matter & { status: MatterStatus; client: Client; lastUsed: Date })[]
+  > {
+    const matters = await this.prismaClient.matter.findMany({
       include: {
         status: true,
         client: true,
+        comments: {
+          take: 1,
+          orderBy: {
+            createdAt: 'desc',
+          },
+        },
+        internalNotes: {
+          take: 1,
+          orderBy: {
+            createdAt: 'desc',
+          },
+        },
       },
+    });
+
+    return matters.map((m) => {
+      let maxDate = new Date(
+        Math.max(
+          ...[
+            m.updatedAt.getTime(),
+            m.comments[0]?.createdAt?.getTime() || -1,
+            m.internalNotes[0]?.createdAt?.getTime() || -1,
+          ],
+        ),
+      );
+
+      delete m.comments;
+      delete m.internalNotes;
+      return {
+        ...m,
+        lastUsed: maxDate,
+      };
     });
   }
 
