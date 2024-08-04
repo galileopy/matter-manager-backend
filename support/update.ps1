ed9b971ddafef67a867e4a2250040bbf1e94a925
martin lego <martintmlg@gmail.com>
2024-08-04 03:43:46 -0400

    chore: Refactor update.ps1 script for GenMatter service
    - Added new functions: StopSeriverce, InstallPm2AndStart, InstallGenMatter, Show-Menu, Handle-Choice
    - Updated existing functions: CreateProxy, GitPullAndNpmBuild
    - Removed unnecessary code and comments
    - Improved error handling and messaging

--- support/update.ps1
+++ support/update.ps1
@@ -5,6 +5,7 @@ $directoryB = "C:\GenMatter\matter-manager-frontend"
 
 $sitePathA = "C:\GenMatter\matter-manager-backend\dist"
 $proxyDestinationA = "http://localhost:3000"
+$genMatterServiceName = "GenMatter"
 
  
 
@@ -94,14 +95,183 @@ function CreateProxy {
 
 }
 
+function StopSeriverce {
+    param (
+        [string]$serviceName
+    )
 
-# Main script execution
-# Step 1: Git pull and npm build for directory A
-GitPullAndNpmBuild -directory $directoryA
+    try {
+        if (-not (Get-Command pm2 -ErrorAction SilentlyContinue)) {
+            throw "pm2 is not installed nothing to stop"
+        }
+    } catch {
+        Write-Host "$($_.Exception.Message)"
+        return
+    }
 
-# Step 2: Git pull and npm build for directory B
-GitPullAndNpmBuild -directory $directoryB
+    try {
+        Write-Host "Stopping GenMatter service"
+        pm2 infmo $serviceName
+        pm2 stop $serviceName
+        pm2 delete $serviceName
+        pm2 list
+        if ($LASTEXITCODE -ne 0) {
+            throw "pm2 stop GenMatter failed"
+        }
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+        return
+    }
+}
 
-# Step 3: Create proxy for Site A
-CreateProxy -sitePath $sitePathA -proxyDestination $proxyDestinationA
-Write-Host "Script execution completed."
+function InstallPm2AndStart {
+    param (
+        [string]$directory,
+        [string]$serviceName
+    )
+
+    try {
+        if (-not (Get-Command pm2 -ErrorAction SilentlyContinue)) {
+            Write-Host "PM2 not installer, installing now"
+            npm install pm2 -g
+            if ($LASTEXITCODE -ne 0) {
+                throw "npm install pm2 -g failed in directory $directory"
+            }
+        }
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+        return
+    }
+
+    try {
+        if (-not (Get-Command pm2-startup -ErrorAction SilentlyContinue)) {
+            Write-Host "pm2-windows-startup not installed, installing now"
+            npm install pm2-windows-startup -g
+            pm2-startup install
+            if ($LASTEXITCODE -ne 0) {
+                throw "npm install pm2-windows-startup -g failed in directory $directory"
+            }
+        }
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+        return
+    }
+    try {
+         pm2 start $directory\src\main.js --name "$serviceName" --watch
+        if ($LASTEXITCODE -ne 0) {
+            throw " pm2 start $directory\src\main.js --name $serviceName --watch failed in directory $directory"
+        }
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+        return
+    }
+    try {
+         pm2 save
+        if ($LASTEXITCODE -ne 0) {
+            throw " pm2 start $directory\dist\main.js --name $serviceName --watch failed in directory $directory"
+        }
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+        return
+    }
+    
+}
+
+function InstallGenMatter {
+    param (
+        [string]$serviceName,
+        [string]$directoryA,
+        [string]$directoryB,
+        [string]$sitePath,
+        [string]$proxyDestination
+    )
+    # Main script execution
+    # Stop the service on pm2 if it installed
+    StopSeriverce -serviceName $serviceName
+
+    # Step 2: Git pull and npm build for directory A
+    GitPullAndNpmBuild -directory $directoryA
+
+    # Step 3: Git pull and npm build for directory B
+    GitPullAndNpmBuild -directory $directoryB
+    Set-Location -Path $directoryA\support
+
+    # Step 4: Install pm2 and start the service
+    InstallPm2AndStart -directory $sitePath -serviceName $serviceName
+
+    # Step 4: Create proxy for Site A
+    CreateProxy -sitePath $sitePath -proxyDestination $proxyDestination
+    Write-Host "Script execution completed."
+}
+
+function Show-Menu {
+    Clear-Host
+    Write-Host "===================================="
+    Write-Host "          Select an option          "
+    Write-Host "===================================="
+    Write-Host "1: Install/Update GenMAtter"
+    Write-Host "2: Start Service"
+    Write-Host "3: Stop Service"
+    Write-Host "4: Log Service"
+    Write-Host "5: Exit"
+}
+
+function Handle-Choice {
+    param (
+        [int]$choice,
+        [string]$serviceName,
+        [string]$directoryA,
+        [string]$directoryB,
+        [string]$sitePath,
+        [string]$proxyDestination
+    )
+
+    switch ($choice) {
+        1 { 
+            InstallGenMatter -serviceName $serviceName -directoryA $directoryA -directoryB $directoryB -sitePath $sitePath -proxyDestination $proxyDestination
+         }
+        2 { 
+            InstallPm2AndStart -directory $sitePath -serviceName $serviceName
+         }
+        3 {
+            StopSeriverce -serviceName $serviceName
+        }
+        4 {
+            pm2 logs $serviceName
+        }
+        default { Write-Host "Saliendo..." }
+    }
+}
+
+# Pause function definition
+function Pause {
+    Write-Host "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+# Main script logic
+$choice = 0
+Show-Menu
+[int]$choice = Read-Host "Select one options"
+Handle-Choice -choice $choice -serviceName $genMatterServiceName -directoryA $directoryA -directoryB $directoryB -sitePath $sitePathA -proxyDestination $proxyDestinationA
+Pause # Pauses the script so the user can see the output before clearing the screen
+
+
+
+# # Main script execution
+# # Stop the service on pm2 if it installed
+# StopSeriverce -serviceName $genMatterServiceName
+
+# # Step 2: Git pull and npm build for directory A
+# GitPullAndNpmBuild -directory $directoryA
+
+# # Step 3: Git pull and npm build for directory B
+# GitPullAndNpmBuild -directory $directoryB
+# Set-Location -Path $directoryA\support
+
+# # Step 4: Install pm2 and start the service
+# InstallPm2AndStart -directory $sitePathA -serviceName $genMatterServiceName
+
+# # Step 4: Create proxy for Site A
+# CreateProxy -sitePath $sitePathA -proxyDestination $proxyDestinationA
+# Write-Host "Script execution completed."
